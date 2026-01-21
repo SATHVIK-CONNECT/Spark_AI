a2f14f2dd75ca726aca5facc39dff80fc9bc3052
Sathvik <sathvikpalivela0@gmail.com>
2026-01-21 13:30:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ import tempfile
 st.set_page_config(page_title="Spark AI", page_icon="⚡")
 
 # Set up Groq API Key
-os.environ['GROQ_API_KEY'] = GROQ_API
+groq_api_key = GROQ_API
 
 # Styling
 canvas = st.markdown("""
@@ -63,9 +63,9 @@ tab_titles = [
 vision = [
     "The vision instruct system utilizes visual instructional RAG to generate interactive and immersive visual instructions. By incorporating computer vision-based instructional RAG, the system can analyze images and generate corresponding instructions. With the integration of multimodal RAG for visual instruction, users can interact with the system using multiple modes of input, enhancing their understanding and retention of complex concepts.\n\nHow to use Vision Instruct\n\n1. Go to Vision Instruct tab.\n\n2. Click on upload option and upload the image.\n\n3. Give the prompt (Question).\n\n4. Click on Generate option."
     ]     
-#file = [
-    #"The file query system utilizes document-centric querying with RAG to retrieve relevant information from PDF documents. By incorporating PDF-based knowledge retrieval with RAG, the system can analyze the content of PDF files and generate answers to user queries. With the integration of file-based question answering with RAG, users can ask questions about the content of files and receive accurate and relevant responses.\n\nHow to use File Query\n\n1. Go to File Query tab.\n\n2. Click on upload option and upload the PDF File.\n\n3. Give the prompt (Question).\n\n4. Click on Generate option."
-   # ]     
+file = [
+    "The file query system utilizes document-centric querying with RAG to retrieve relevant information from PDF documents. By incorporating PDF-based knowledge retrieval with RAG, the system can analyze the content of PDF files and generate answers to user queries. With the integration of file-based question answering with RAG, users can ask questions about the content of files and receive accurate and relevant responses.\n\nHow to use File Query\n\n1. Go to File Query tab.\n\n2. Click on upload option and upload the PDF File.\n\n3. Give the prompt (Question).\n\n4. Click on Generate option."
+   ]     
 
 tabs = st.tabs(tab_titles)
 with tabs[0]:
@@ -108,12 +108,12 @@ with tabs[0]:
     with st.expander("V I S I O N - I N S T R U C T"):
         st.write(vision[0])
 
-    #st.markdown("""
-    #    <h5>File Query</h5>
-    #    <p style="text-align: justify;">It is used to query with files. It let us analyze the files like PDF, TXT and so on by using the llama model.</p>
-  #  """, unsafe_allow_html=True)
-  #  with st.expander("F I L E - Q U E R Y"):
-     #   st.write(file[0])
+    st.markdown("""
+       <h5>File Query</h5>
+       <p style="text-align: justify;">It is used to query with files. It let us analyze the files like PDF, TXT and so on by using the llama model.</p>
+   """, unsafe_allow_html=True)
+   with st.expander("F I L E - Q U E R Y"):
+       st.write(file[0])
 
 with tabs[1]:
     #upload file
@@ -138,98 +138,98 @@ with tabs[1]:
                         output = generate(uploaded_file, 'What is in this picture?')
                 st.subheader('Result:')
                 st.write(output)
- # with tabs[2]:        
-   # def pdf_analyze(pdf_analyze="pdf_analyze.json"):
-      #  with open(pdf_analyze, "r", encoding='UTF-8') as f:
-         #   return json.load(f)
- #   pdf_analyze = pdf_analyze()
-  #  st_lottie(pdf_analyze)   
+ with tabs[2]:        
+   def pdf_analyze(pdf_analyze="pdf_analyze.json"):
+       with open(pdf_analyze, "r", encoding='UTF-8') as f:
+           return json.load(f)
+   pdf_analyze = pdf_analyze()
+   st_lottie(pdf_analyze)   
 
 
-  #  groq_api_key = os.getenv('GROQ_API_KEY')
+   groq_api_key = GROQ_API
 
-   # llm = ChatGroq(groq_api_key=groq_api_key, model_name="Llama3-8b-8192")
+   llm = ChatGroq(groq_api_key=groq_api_key, model_name="Llama3-8b-8192")
 
 
-   # prompt = ChatPromptTemplate.from_template(
+   prompt = ChatPromptTemplate.from_template(
     
-     #   Answer the questions based on the provided context only.
-     #   Please provide the most accurate response based on the question.
-      #  <context>
-      #  {context}
-     #   <context>
-      #  Questions: {input}
+       Answer the questions based on the provided context only.
+       Please provide the most accurate response based on the question.
+       <context>
+       {context}
+       <context>
+       Questions: {input}
         
- #   )
+   )
 
-    #def create_vector_db_out_of_the_uploaded_pdf_file(pdf_file):
-
-
-       # if "vector_store" not in st.session_state:
+    def create_vector_db_out_of_the_uploaded_pdf_file(pdf_file):
 
 
-         #   with tempfile.NamedTemporaryFile(delete=False) as temp_file:
+       if "vector_store" not in st.session_state:
 
-             #   temp_file.write(pdf_file.read())
 
-              #  pdf_file_path = temp_file.name
+           with tempfile.NamedTemporaryFile(delete=False) as temp_file:
 
-          #  st.session_state.embeddings = HuggingFaceEmbeddings(model_name='BAAI/bge-small-en-v1.5', model_kwargs={'device': 'cpu'}, encode_kwargs={'normalize_embeddings': True})
+               temp_file.write(pdf_file.read())
+
+               pdf_file_path = temp_file.name
+
+           st.session_state.embeddings = HuggingFaceEmbeddings(model_name='BAAI/bge-small-en-v1.5', model_kwargs={'device': 'cpu'}, encode_kwargs={'normalize_embeddings': True})
             
-           # st.session_state.loader = PyPDFLoader(pdf_file_path)
+           st.session_state.loader = PyPDFLoader(pdf_file_path)
 
-          #  st.session_state.text_document_from_pdf = st.session_state.loader.load()
+           st.session_state.text_document_from_pdf = st.session_state.loader.load()
 
-         #   st.session_state.text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+           st.session_state.text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
             
-          #  st.session_state.final_document_chunks = st.session_state.text_splitter.split_documents(st.session_state.text_document_from_pdf)
+           st.session_state.final_document_chunks = st.session_state.text_splitter.split_documents(st.session_state.text_document_from_pdf)
 
-           # st.session_state.vector_store = FAISS.from_documents(st.session_state.final_document_chunks, st.session_state.embeddings)
-
-
-   # pdf_input_from_user = st.file_uploader("Upload the PDF file", type=['pdf'])
+           st.session_state.vector_store = FAISS.from_documents(st.session_state.final_document_chunks, st.session_state.embeddings)
 
 
-   # if pdf_input_from_user is not None:
-      #  with st.spinner('Generating output...'):
+   pdf_input_from_user = st.file_uploader("Upload the PDF file", type=['pdf'])
+
+
+   if pdf_input_from_user is not None:
+       with st.spinner('Generating output...'):
     
-          #  if pdf_input_from_user is not None:
+           if pdf_input_from_user is not None:
 
-            #    create_vector_db_out_of_the_uploaded_pdf_file(pdf_input_from_user)
+               create_vector_db_out_of_the_uploaded_pdf_file(pdf_input_from_user)
             
-         #   else:
+           else:
                 
-              #  st.write("Please upload a PDF file first")
+               st.write("Please upload a PDF file first")
 
 
 
-  #  if "vector_store" in st.session_state:
+   if "vector_store" in st.session_state:
 
-      #  user_prompt = st.text_input("Enter Your Question related to the uploaded PDF")
+       user_prompt = st.text_input("Enter Your Question related to the uploaded PDF")
 
-       # if st.button('Generate Analysis'):
-              #  if user_prompt: 
-                  #  if "vector_store" in st.session_state:
+       if st.button('Generate Analysis'):
+               if user_prompt: 
+                   if "vector_store" in st.session_state:
 
-                    #    document_chain = create_stuff_documents_chain(llm, prompt)
+                       document_chain = create_stuff_documents_chain(llm, prompt)
 
-                    #    retriever = st.session_state.vector_store.as_retriever()
+                       retriever = st.session_state.vector_store.as_retriever()
 
-                       # retrieval_chain = create_retrieval_chain(retriever, document_chain)
+                       retrieval_chain = create_retrieval_chain(retriever, document_chain)
 
-                      #  response = retrieval_chain.invoke({'input': user_prompt})
+                       response = retrieval_chain.invoke({'input': user_prompt})
 
-                    #    st.write(response['answer'])
+                       st.write(response['answer'])
 
-                  #  else:   
+                   else:   
 
-                     #   st.write("Please embed the document first by uploading a PDF file.")
+                       st.write("Please embed the document first by uploading a PDF file.")
 
-             #   else:
+               else:
 
-                   # st.error('Please write your prompt')
+                   st.error('Please write your prompt')
 
-with tabs[2]:
+with tabs[3]:
     #upload file
     st.markdown("""
         <h4>About Spark AI</h4>
